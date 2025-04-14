2659aa59ae6d95a041b441b77ef47360c4a29dee
Alanna Ramsoondar <110500317+alannaramm@users.noreply.github.com>
2025-04-14 16:28:47 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,77 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="background-cover" :style="{ backgroundImage: `url(${bgImage})` }">
+    <div class="welcome-box">
+      <input v-model="name" type="text" placeholder="Enter your name" class="input-box" />
+      <button class="welcome-button" @click="showWelcome = true">Welcome</button>
+      <p v-if="showWelcome" class="greeting">Welcome, {{ name }}!</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+import bgImage from '@/assets/journal-cover.jpg'
+
+const name = ref('')
+const showWelcome = ref(false)
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.background-cover {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.greeting {
+  margin-top: 1rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #f54291; 
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+
+.welcome-box {
+  background-color:transparent;
+  padding: 2rem;
+  border-radius: 12px;
+  text-align: center;
+  margin-top: -300px;
+
+
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.input-box {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  border: 2px solid #f54291;
+  border-radius: 8px;
+  margin-bottom: 1rem;
+  width: 100%;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.welcome-button {
+  padding: 0.5rem 1.5rem;
+  font-size: 1rem;
+  border: 2px solid #333;
+  border-radius: 8px;
+  background-color: rgb(45, 36, 36);
+  cursor: pointer;
+  transition: 0.3s;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+
+.greeting {
+  margin-top: 1rem;
+  font-size: 1.2rem;
+  font-weight: bold;
 }
 </style>
+
